504817c91501972482468789f13aa82f49cc3b86
Henry Heino <henryuheino@gmail.com>
2024-05-13 15:04:31 -0700

    TEST: ECB mode RSA decrypt chaining

--- packages/app-mobile/services/e2ee/RSA.react-native.ts
+++ packages/app-mobile/services/e2ee/RSA.react-native.ts
@@ -27,7 +27,27 @@ const rsa: RSA = {
 	},
 
 	decrypt: async (ciphertextBase64: string, rsaKeyPair: RSAKeyPair): Promise<string> => {
-		return RnRSA.decrypt(ciphertextBase64, rsaKeyPair.private);
+		const ctBuffer = Buffer.from(ciphertextBase64, 'base64');
+		const pkLength = Buffer.from(rsaKeyPair.private, 'base64').byteLength;
+		console.log('pkLength', pkLength, Buffer.from(rsaKeyPair.public, 'base64').byteLength);
+		if (ctBuffer.length > 256) {
+			const maxMessageLength = 256;// - 11; // -11: Padding
+			const numBlocks = Math.ceil(ctBuffer.length / maxMessageLength);
+			const blockSize = ctBuffer.length / numBlocks;
+
+			const result: string[] = [];
+			for (let i = 0; i < numBlocks; i++) {
+				const ctBlock = ctBuffer.slice(i * blockSize, Math.min(ctBuffer.length, (i + 1) * blockSize));
+				const plainText = await RnRSA.decrypt(ctBlock.toString('base64'), rsaKeyPair.private);
+				if (plainText === undefined) {
+					throw new Error(`RN RSA: Decryption failed.`);
+				}
+				result.push(plainText);
+			}
+			return result.join('');
+		} else {
+			return RnRSA.decrypt(ciphertextBase64, rsaKeyPair.private);
+		}
 	},
 
 	publicKey: (rsaKeyPair: RSAKeyPair): string => {
