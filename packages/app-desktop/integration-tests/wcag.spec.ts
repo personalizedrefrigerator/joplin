c1e65dcc5778be127fd7e2af6598642747bbcd56
Henry Heino <personalizedrefrigerator@gmail.com>
2024-11-08 16:03:09 -0800

    Fixing tests

--- packages/app-desktop/integration-tests/wcag.spec.ts
+++ packages/app-desktop/integration-tests/wcag.spec.ts
@@ -53,10 +53,11 @@ test.describe('wcag', () => {
 		const mainScreen = new MainScreen(mainWindow);
 		await mainScreen.waitFor();
 
+		await mainScreen.createNewNote('Test');
+
 		// For now, activate all notes to make it active. When inactive, it causes a contrast warning.
 		// This seems to be allowed under WCAG 2.2 SC 1.4.3 under the "Incidental" exception.
 		await mainScreen.sidebar.allNotes.click();
-		await mainScreen.createNewNote('Test');
 
 		// Ensure that `:hover` styling is consistent between tests:
 		await mainScreen.noteEditor.noteTitleInput.hover();
