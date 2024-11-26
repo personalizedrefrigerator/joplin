d2308bfeed10a7a064c609d180df619e6a7ea078
Henry Heino <personalizedrefrigerator@gmail.com>
2024-11-26 12:18:41 -0800

    Remove tab override from the notebook sidebar (now jumps to the sync button)

--- packages/app-desktop/gui/Sidebar/hooks/useOnSidebarKeyDownHandler.ts
+++ packages/app-desktop/gui/Sidebar/hooks/useOnSidebarKeyDownHandler.ts
@@ -54,14 +54,10 @@ const useOnSidebarKeyDownHandler = (props: Props) => {
 			indexChange = -1;
 		} else if (event.code === 'ArrowDown') {
 			indexChange = 1;
-		} else if (event.code === 'Tab') {
+		} else if (event.code === 'Tab' && event.shiftKey) {
 			event.preventDefault();
 
-			if (event.shiftKey) {
-				void CommandService.instance().execute('focusElement', 'noteBody');
-			} else {
-				void CommandService.instance().execute('focusElement', 'noteList');
-			}
+			void CommandService.instance().execute('focusElement', 'noteBody');
 		}
 
 		if (indexChange !== 0) {
