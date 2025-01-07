1a266afceb83dcc7a77ed5103087ab80c806279b
Henry Heino <personalizedrefrigerator@gmail.com>
2025-01-07 14:14:43 -0800

    Reset unnecessary changes
    Some changes were unrelated to this pull request

--- packages/lib/Synchronizer.ts
+++ packages/lib/Synchronizer.ts
@@ -337,10 +337,8 @@ export default class Synchronizer {
 		const hasActiveExclusiveLock = await hasActiveLock(locks, currentDate, this.lockHandler().lockTtl, LockType.Exclusive);
 		if (hasActiveExclusiveLock) return 'hasExclusiveLock';
 
-		if (this.lockHandler().enabled) {
-			const hasActiveSyncLock = await hasActiveLock(locks, currentDate, this.lockHandler().lockTtl, LockType.Sync, this.lockClientType(), this.clientId_);
-			if (!hasActiveSyncLock) return 'syncLockGone';
-		}
+		const hasActiveSyncLock = await hasActiveLock(locks, currentDate, this.lockHandler().lockTtl, LockType.Sync, this.lockClientType(), this.clientId_);
+		if (!hasActiveSyncLock) return 'syncLockGone';
 
 		return '';
 	}
