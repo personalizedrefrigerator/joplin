d3f0e1c57a9b36247092645d307e9613c0391754
Henry Heino <personalizedrefrigerator@gmail.com>
2025-01-24 08:02:30 -0800

    Desktop: Performance: Limit IPC on state change
    Makes synchronous IPC calls happen less frequently.

--- packages/app-desktop/gui/Navigator.tsx
+++ packages/app-desktop/gui/Navigator.tsx
@@ -1,50 +1,64 @@
 import * as React from 'react';
-const { connect } = require('react-redux');
+import { connect } from 'react-redux';
 import Setting from '@joplin/lib/models/Setting';
 import { AppState, AppStateRoute } from '../app.reducer';
 import bridge from '../services/bridge';
-import { useContext, useEffect, useRef } from 'react';
+import { useContext, useEffect, useMemo, useRef } from 'react';
 import { WindowIdContext } from './NewWindowOrIFrame';
+
+interface AppScreen {
+	screen: React.ComponentType;
+	title?: ()=> string;
+}
 
 interface Props {
 	route: AppStateRoute;
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any -- Old code before rule was applied
-	screens: Record<string, any>;
+	screens: Record<string, AppScreen>;
 
 	style: React.CSSProperties;
 	className?: string;
 }
 
-const NavigatorComponent: React.FC<Props> = props => {
+const useWindowTitleManager = (screenInfo: AppScreen) => {
+	const windowTitle = useMemo(() => {
+		const devMarker = Setting.value('env') === 'dev' ? ` (DEV - ${Setting.value('profileDir')})` : '';
+		const windowTitle = [`Joplin${devMarker}`];
+		if (screenInfo?.title) {
+			windowTitle.push(screenInfo.title());
+		}
+		return windowTitle.join(' - ');
+	}, [screenInfo]);
+
 	const windowId = useContext(WindowIdContext);
+	useEffect(() => {
+		bridge().windowById(windowId)?.setTitle(windowTitle);
+	}, [windowTitle, windowId]);
+};
+
+const useWindowRefocusManager = (route: AppStateRoute) => {
+	const windowId = useContext(WindowIdContext);
+
+	const prevRouteName = useRef<string|null>(null);
+	const routeName = route?.routeName;
+	useEffect(() => {
+		// When a navigation happens in an unfocused window, show the window to the user.
+		// This might happen if, for example, a secondary window triggers a navigation in
+		// the main window.
+		if (routeName && routeName !== prevRouteName.current) {
+			bridge().switchToWindow(windowId);
+		}
+
+		prevRouteName.current = routeName;
+	}, [routeName, windowId]);
+};
+
+const NavigatorComponent: React.FC<Props> = props => {
 
 	const route = props.route;
 	const screenInfo = props.screens[route?.routeName];
 
-	const screensRef = useRef(props.screens);
-	screensRef.current = props.screens;
-
-	const prevRoute = useRef<AppStateRoute|null>(null);
-	useEffect(() => {
-		const routeName = route?.routeName;
-		if (route) {
-			const devMarker = Setting.value('env') === 'dev' ? ` (DEV - ${Setting.value('profileDir')})` : '';
-			const windowTitle = [`Joplin${devMarker}`];
-			if (screenInfo.title) {
-				windowTitle.push(screenInfo.title());
-			}
-			bridge().windowById(windowId)?.setTitle(windowTitle.join(' - '));
-		}
-
-		// When a navigation happens in an unfocused window, show the window to the user.
-		// This might happen if, for example, a secondary window triggers a navigation in
-		// the main window.
-		if (routeName && routeName !== prevRoute.current?.routeName) {
-			bridge().switchToWindow(windowId);
-		}
-
-		prevRoute.current = route;
-	}, [route, screenInfo, windowId]);
+	useWindowTitleManager(screenInfo);
+	useWindowRefocusManager(route);
 
 	if (!route) throw new Error('Route must not be null');
 
