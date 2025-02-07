01e7095cc722d94baf374d15a8be45d2b24545cc
Henry Heino <personalizedrefrigerator@gmail.com>
2025-02-07 13:00:51 -0800

    Chore: Web: Support hot module reloading in dev mode
    This commit should make developing with the web app easier. It:
    - Enables hot React component replacement with
      react-refresh-webpack-plugin.
    - Disables the service worker in dev mode.
      - The service worker is responsible for 1) ensuring that only one copy
        of the web app is open at a time and 2) setting HTTP headers
        necessary for using sqlite-wasm. It's only necessary when not using
        webpack-dev-server.

--- packages/app-mobile/web/webpack.config.js
+++ packages/app-mobile/web/webpack.config.js
@@ -5,33 +5,7 @@
 const path = require('path');
 const appDirectory = path.resolve(__dirname, '../');
 const babelConfig = require('../babel.config');
-
-const babelLoaderConfiguration = {
-	test: /\.(tsx|jsx|ts|js|mjs)$/,
-	exclude: [
-		path.resolve(appDirectory, 'ios'),
-		path.resolve(appDirectory, 'android'),
-
-		// Compiling these libraries with babel cause build errors.
-		/.*node_modules[/\\]@babel.*/,
-		/.*node_modules[/\\]@sqlite\.org[/\\].*/,
-		/.*node_modules[/\\]markdown-it-anchor[/\\].*/,
-		/.*node_modules[/\\]markdown-it-toc-done-right[/\\].*/,
-	],
-
-	use: {
-		loader: 'babel-loader',
-		options: {
-			cacheDirectory: true,
-			presets: babelConfig.presets,
-			plugins: [
-				'react-native-web',
-				'@babel/plugin-transform-export-namespace-from',
-				...(babelConfig.plugins ?? []),
-			],
-		},
-	},
-};
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const resourceLoaderConfiguration = {
 	test: /\.(gif|jpe?g|png|svg|ttf)$/,
@@ -40,80 +14,121 @@ const resourceLoaderConfiguration = {
 
 const emptyLibraryMock = path.resolve(__dirname, 'mocks/empty.js');
 
-module.exports = {
-	target: 'web',
+module.exports = (env) => {
+	const isDevMode = !env.production;
 
-	entry: {
-		app: path.resolve(appDirectory, 'index.web.ts'),
-		serviceWorker: path.resolve(appDirectory, 'web/serviceWorker.ts'),
-	},
+	const babelLoaderConfiguration = {
+		test: /\.(tsx|jsx|ts|js|mjs)$/,
+		exclude: [
+			path.resolve(appDirectory, 'ios'),
+			path.resolve(appDirectory, 'android'),
 
-	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(appDirectory, 'web/dist'),
-	},
 
-	module: {
-		rules: [
-			babelLoaderConfiguration,
-			resourceLoaderConfiguration,
-		],
-	},
-
-	resolve: {
-		alias: {
-			'react-native$': 'react-native-web',
-			'crypto': path.resolve(__dirname, 'mocks/nodeCrypto.js'),
-
-			// Map some modules that don't work on web to the empty dictionary.
-			'react-native-fingerprint-scanner': emptyLibraryMock,
-			'@joplin/react-native-saf-x': emptyLibraryMock,
-			'react-native-quick-actions': emptyLibraryMock,
-			'uglifycss': emptyLibraryMock,
-			'react-native-share': emptyLibraryMock,
-			'react-native-camera': emptyLibraryMock,
-			'react-native-zip-archive': emptyLibraryMock,
-			'react-native-document-picker': emptyLibraryMock,
-			'react-native-exit-app': emptyLibraryMock,
-			'expo-camera': emptyLibraryMock,
-
-			// Workaround for applying serviceworker types to a single file.
-			// See https://joshuatz.com/posts/2021/strongly-typed-service-workers/.
-			// See https://github.com/microsoft/TypeScript/issues/37053
-			'serviceworker': emptyLibraryMock,
-		},
-		// Prefers .web.js, .web.ts, etc. imports to other imports.
-		extensions: [
-			'.web.js',
-			'.js',
-			'.web.ts',
-			'.ts',
-			'.web.mjs',
-			'.mjs',
-			'.web.tsx',
-			'.tsx',
-			'.web.jsx',
-			'.jsx',
-			'.wasm',
+			// Compiling these libraries with babel cause build errors.
+			/.*node_modules[/\\]@babel.*/,
+			/.*node_modules[/\\]@sqlite\.org[/\\].*/,
+			/.*node_modules[/\\]markdown-it-anchor[/\\].*/,
+			/.*node_modules[/\\]markdown-it-toc-done-right[/\\].*/,
 		],
 
-		fallback: {
-			'url': require.resolve('url/'),
-			'events': require.resolve('events/'),
-			'timers': require.resolve('timers-browserify'),
-			'path': require.resolve('path-browserify'),
-			'stream': require.resolve('stream-browserify'),
-			'crypto': require.resolve('crypto-browserify'),
+		use: {
+			loader: 'babel-loader',
+			options: {
+				cacheDirectory: true,
+				presets: babelConfig.presets,
+				plugins: [
+					'react-native-web',
+					'@babel/plugin-transform-export-namespace-from',
+					...(babelConfig.plugins ?? []),
+					...(isDevMode ? ['react-refresh/babel'] : []),
+				],
+			},
 		},
-	},
+	};
 
-	devServer: {
-		// Required by @sqlite.org/sqlite-wasm
-		// See https://www.npmjs.com/package/@sqlite.org/sqlite-wasm#user-content-in-a-wrapped-worker-with-opfs-if-available
-		headers: {
-			'Cross-Origin-Opener-Policy': 'same-origin',
-			'Cross-Origin-Embedder-Policy': 'require-corp',
+	return {
+		mode: isDevMode ? 'development' : 'production',
+		target: 'web',
+
+		entry: {
+			app: path.resolve(appDirectory, 'index.web.ts'),
+			...(isDevMode ? {} : {
+				// The service worker can cause issues with live reload in dev mode
+				// (and isn't necessary when using webpack-dev-server).
+				serviceWorker: path.resolve(appDirectory, 'web/serviceWorker.ts'),
+			}),
 		},
-		port: 8088,
-	},
+
+		output: {
+			filename: '[name].bundle.js',
+			path: path.resolve(appDirectory, 'web/dist'),
+		},
+
+		module: {
+			rules: [
+				babelLoaderConfiguration,
+				resourceLoaderConfiguration,
+			],
+		},
+		plugins: isDevMode ? [
+			new ReactRefreshWebpackPlugin(),
+		] : [],
+
+		resolve: {
+			alias: {
+				'react-native$': 'react-native-web',
+				'crypto': path.resolve(__dirname, 'mocks/nodeCrypto.js'),
+
+				// Map some modules that don't work on web to the empty dictionary.
+				'react-native-fingerprint-scanner': emptyLibraryMock,
+				'@joplin/react-native-saf-x': emptyLibraryMock,
+				'react-native-quick-actions': emptyLibraryMock,
+				'uglifycss': emptyLibraryMock,
+				'react-native-share': emptyLibraryMock,
+				'react-native-camera': emptyLibraryMock,
+				'react-native-zip-archive': emptyLibraryMock,
+				'react-native-document-picker': emptyLibraryMock,
+				'react-native-exit-app': emptyLibraryMock,
+				'expo-camera': emptyLibraryMock,
+
+				// Workaround for applying serviceworker types to a single file.
+				// See https://joshuatz.com/posts/2021/strongly-typed-service-workers/.
+				// See https://github.com/microsoft/TypeScript/issues/37053
+				'serviceworker': emptyLibraryMock,
+			},
+			// Prefers .web.js, .web.ts, etc. imports to other imports.
+			extensions: [
+				'.web.js',
+				'.js',
+				'.web.ts',
+				'.ts',
+				'.web.mjs',
+				'.mjs',
+				'.web.tsx',
+				'.tsx',
+				'.web.jsx',
+				'.jsx',
+				'.wasm',
+			],
+
+			fallback: {
+				'url': require.resolve('url/'),
+				'events': require.resolve('events/'),
+				'timers': require.resolve('timers-browserify'),
+				'path': require.resolve('path-browserify'),
+				'stream': require.resolve('stream-browserify'),
+				'crypto': require.resolve('crypto-browserify'),
+			},
+		},
+
+		devServer: {
+			// Required by @sqlite.org/sqlite-wasm
+			// See https://www.npmjs.com/package/@sqlite.org/sqlite-wasm#user-content-in-a-wrapped-worker-with-opfs-if-available
+			headers: {
+				'Cross-Origin-Opener-Policy': 'same-origin',
+				'Cross-Origin-Embedder-Policy': 'require-corp',
+			},
+			port: 8088,
+		},
+	};
 };
