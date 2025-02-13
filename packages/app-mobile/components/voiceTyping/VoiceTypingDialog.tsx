2cd33c5b75c62694c9c87cc4d20da328e96862d2
Henry Heino <personalizedrefrigerator@gmail.com>
2025-02-12 18:49:54 -0800

    Only allow switching to speech-to-text on supported platforms

--- packages/app-mobile/components/voiceTyping/VoiceTypingDialog.tsx
+++ packages/app-mobile/components/voiceTyping/VoiceTypingDialog.tsx
@@ -4,14 +4,20 @@ import { Surface, SegmentedButtons } from 'react-native-paper';
 import { _ } from '@joplin/lib/locale';
 import { AppState } from '../../utils/types';
 import { connect } from 'react-redux';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 import SpeechToText from './SpeechToText';
 import { OnFileSavedCallback } from './types';
 import AudioRecording from './AudioRecording';
 
+enum VoiceTypingMode {
+	AudioRecording = 'record',
+	SpeechToText = 'speech-to-text',
+}
+
 interface Props {
 	locale: string;
 	provider: string;
+	defaultVoiceTypingMode: VoiceTypingMode;
 	onDismiss: ()=> void;
 	onText: (text: string)=> void;
 	onFile: OnFileSavedCallback;
@@ -26,48 +32,57 @@ const styles = StyleSheet.create({
 	},
 });
 
-enum VoiceTypingMode {
-	AudioRecording = 'record',
-	SpeechToText = 'speech-to-text',
-}
+const supportsSpeechToText = Platform.OS === 'android';
 
 const VoiceTypingDialog: React.FC<Props> = props => {
-	const [voiceTypingMode, setVoiceTypingMode] = useState<VoiceTypingMode>(null);
+	const [voiceTypingMode, setVoiceTypingMode] = useState<VoiceTypingMode>(props.defaultVoiceTypingMode);
 	let content;
-	if (voiceTypingMode === VoiceTypingMode.SpeechToText) {
+	if (voiceTypingMode === VoiceTypingMode.AudioRecording || !supportsSpeechToText) {
+		content = <AudioRecording
+			onFileSaved={props.onFile}
+			onDismiss={props.onDismiss}
+		/>;
+	} else if (voiceTypingMode === VoiceTypingMode.SpeechToText) {
 		content = <SpeechToText
 			locale={props.locale}
 			onText={props.onText}
 			onDismiss={props.onDismiss}
 		/>;
-	} else if (voiceTypingMode === VoiceTypingMode.AudioRecording) {
-		content = <AudioRecording
-			onFileSaved={props.onFile}
-			onDismiss={props.onDismiss}
-		/>;
 	}
+
+	const modeSelector = <SegmentedButtons
+		value={voiceTypingMode}
+		onValueChange={(value: string) => setVoiceTypingMode(value as VoiceTypingMode)}
+		buttons={[
+			{
+				value: VoiceTypingMode.AudioRecording,
+				label: _('Voice recording'),
+			},
+			{
+				value: VoiceTypingMode.SpeechToText,
+				label: _('Speech to text'),
+			},
+		]}
+	/>;
 
 	return <Surface>
 		<View style={styles.container}>
 			{content}
-			<SegmentedButtons
-				value={voiceTypingMode}
-				onValueChange={(value: string) => setVoiceTypingMode(value as VoiceTypingMode)}
-				buttons={[
-					{
-						value: VoiceTypingMode.AudioRecording,
-						label: _('Voice recording'),
-					},
-					{
-						value: VoiceTypingMode.SpeechToText,
-						label: _('Speech to text'),
-					},
-				]}
-			/>
+			{supportsSpeechToText ? modeSelector : null}
 		</View>
 	</Surface>;
 };
 
-export default connect((state: AppState) => ({
-	provider: state.settings['voiceTyping.preferredProvider'],
-}))(VoiceTypingDialog);
+export default connect((state: AppState) => {
+	let defaultVoiceTypingMode = supportsSpeechToText ? VoiceTypingMode.SpeechToText : VoiceTypingMode.AudioRecording;
+	if (state.settings['voiceTyping.mode'] === VoiceTypingMode.SpeechToText) {
+		defaultVoiceTypingMode = VoiceTypingMode.SpeechToText;
+	} else if (state.settings['voiceTyping.mode'] === VoiceTypingMode.AudioRecording) {
+		defaultVoiceTypingMode = VoiceTypingMode.AudioRecording;
+	}
+
+	return {
+		provider: state.settings['voiceTyping.preferredProvider'],
+		defaultVoiceTypingMode,
+	};
+})(VoiceTypingDialog);
