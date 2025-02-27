cc88902fa4cce0576581929d0a8448bfc30e5c94
Henry Heino <personalizedrefrigerator@gmail.com>
2025-02-27 11:20:10 -0800

    Reapply "Experiment: Try using the new note menu as a popover dialog that marks everything else as intert=true"
    This reverts commit f102eb2b90c02a31c84445a408fdb3f9a7172760.

--- packages/app-mobile/components/sidebar/BottomDrawer.tsx
+++ packages/app-mobile/components/sidebar/BottomDrawer.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import Modal from '../Modal';
+import { connect } from 'react-redux';
 import SideMenu, { SideMenuPosition } from './SideMenu';
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { LayoutChangeEvent, StyleSheet, useWindowDimensions, View } from 'react-native';
+import { LayoutChangeEvent, ScrollView, StyleSheet, useWindowDimensions, View } from 'react-native';
 import { ThemeStyle, themeStyle } from '../global-style';
-import { connect } from 'react-redux';
-import { AppState } from '../../utils/types';
 import useSafeAreaPadding from '../../utils/hooks/useSafeAreaPadding';
+import { AppState } from '../../utils/types';
 
 interface Props {
 	themeId: number;
 	children: React.ReactNode;
 	menuLabel: string;
+	menu: React.ReactNode;
 	show: boolean;
 	onDismiss: ()=> void;
 }
@@ -82,10 +82,6 @@ const BottomDrawer: React.FC<Props> = props => {
 		lastOpen.current = isOpen;
 	}, [isOpen, props.onDismiss]);
 
-	const onModalDismiss = useCallback(() => {
-		setIsOpen(false);
-	}, []);
-
 	const [openMenuOffset, setOpenMenuOffset] = useState(400);
 	const onMenuLayout = useCallback((event: LayoutChangeEvent) => {
 		const height = event.nativeEvent.layout.height;
@@ -94,37 +90,28 @@ const BottomDrawer: React.FC<Props> = props => {
 
 	const theme = themeStyle(props.themeId);
 	const styles = useStyles(theme);
-	const menu = <View style={styles.outerContainer}>
+	const menu = <ScrollView style={styles.outerContainer}>
 		<View onLayout={onMenuLayout} style={styles.contentContainer}>
-			{props.children}
+			{props.menu}
 		</View>
-	</View>;
+	</ScrollView>;
 
-	return <Modal
-		visible={props.show}
-		modalBackgroundStyle={styles.modalBackground}
-		transparent={true}
-		animationType='fade'
-		onDismiss={onModalDismiss}
-		onRequestClose={onModalDismiss}
-	>
-		<SideMenu
-			label={props.menuLabel}
-			menuPosition={SideMenuPosition.Bottom}
-			menu={menu}
-			isOpen={isOpen}
-			onChange={(visible) => {
-				if (!visible) {
-					setIsOpen(false);
-				}
-			}}
+	return <SideMenu
+		label={props.menuLabel}
+		menuPosition={SideMenuPosition.Bottom}
+		menu={menu}
+		isOpen={isOpen}
+		onChange={(visible) => {
+			if (!visible) {
+				setIsOpen(false);
+			}
+		}}
 
-			disableGestures={false}
-			openMenuOffset={openMenuOffset}
-			overlayColor={theme.color}
-			menuStyle={styles.menuStyle}
-		><View/></SideMenu>
-	</Modal>;
+		disableGestures={false}
+		openMenuOffset={openMenuOffset}
+		overlayColor={theme.color}
+		menuStyle={styles.menuStyle}
+	>{props.children}</SideMenu>;
 };
 
 export default connect((state: AppState) => {
