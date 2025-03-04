204730f630650337d21018e9949b11850088714a
Henry Heino <personalizedrefrigerator@gmail.com>
2025-03-04 15:16:26 -0800

    Fix tests

--- packages/app-mobile/components/accessibility/FocusControl/ModalContent.tsx
+++ packages/app-mobile/components/accessibility/FocusControl/ModalContent.tsx
@@ -13,12 +13,15 @@ const ModalContent: React.FC<Props> = props => {
 	const focusControl = useContext(FocusControlContext);
 	const id = useId();
 	useEffect(() => {
+		if (!focusControl) {
+			throw new Error('ModalContent components must have a FocusControlProvider as an ancestor. Is FocusControlProvider part of the provider stack?');
+		}
 		focusControl.setDialogOpen(id, props.visible);
 	}, [id, props.visible, focusControl]);
 
 	useEffect(() => {
 		return () => {
-			focusControl.setDialogOpen(id, false);
+			focusControl?.setDialogOpen(id, false);
 		};
 	}, [id, focusControl]);
 
