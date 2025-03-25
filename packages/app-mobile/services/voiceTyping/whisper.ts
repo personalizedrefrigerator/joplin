318598ab63b7225d44f8dfe02e684fe7ec46a849
Henry Heino <personalizedrefrigerator@gmail.com>
2025-03-25 16:53:13 -0700

    Android: Change default voice typing model to whisper-base-q8_0

--- packages/app-mobile/services/voiceTyping/whisper.ts
+++ packages/app-mobile/services/voiceTyping/whisper.ts
@@ -202,14 +202,14 @@ const whisper: VoiceTypingProvider = {
 		let urlTemplate = rtrimSlashes(Setting.value('voiceTypingBaseUrl').trim());
 
 		if (!urlTemplate) {
-			urlTemplate = 'https://github.com/personalizedrefrigerator/joplin-voice-typing-test/releases/download/v0.0.3/{task}.zip';
+			urlTemplate = 'https://github.com/joplin/voice-typing-models/releases/download/v0.2.0/{task}.zip';
 		}
 
-		// Note: whisper-base-q8_0 is also available and works on many Android devices. On some low
-		// resource devices, however, it will fail.
-		// TODO: Auto-select the model size?
+		// Note: whisper-base-q8_0.fr is also available and may have better performance on French-language
+		// input.
+		// TODO: Auto-select the model?
 		return urlTemplate
-			.replace(/\{task\}/g, 'whisper-tiny-q8_0')
+			.replace(/\{task\}/g, 'whisper-base-q8_0')
 			.replace(/\{lang\}/g, lang);
 	},
 	deleteCachedModels: async (locale) => {
