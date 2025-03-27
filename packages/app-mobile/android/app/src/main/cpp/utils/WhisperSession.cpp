5198b598bbf7577b10488d9569a1782de1492a7c
Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>
2025-03-27 13:57:05 -0700

    Android: Voice typing: Improve transcription at the end of paragraphs (#12013)

--- packages/app-mobile/android/app/src/main/cpp/utils/WhisperSession.cpp
+++ packages/app-mobile/android/app/src/main/cpp/utils/WhisperSession.cpp
@@ -64,7 +64,8 @@ WhisperSession::buildWhisperParams_() {
 
 std::string
 WhisperSession::transcribe_(const std::vector<float>& audio, size_t transcribeCount) {
-	int minTranscribeLength = WHISPER_SAMPLE_RATE / 2; // 0.5s
+	// Whisper won't transcribe anything shorter than 1s.
+	int minTranscribeLength = WHISPER_SAMPLE_RATE; // 1s
 	if (transcribeCount < minTranscribeLength) {
 		return "";
 	}
@@ -152,6 +153,12 @@ WhisperSession::transcribeNextChunk() {
 			audioBuffer_.clear();
 			return false;
 		} else if (splitEnd > tolerance) { // Anything to transcribe?
+			// Include some of the silence between the start and the end. Excluding it
+			// seems to make Whisper more likely to omit trailing punctuation.
+			int maximumSilentSamples = WHISPER_SAMPLE_RATE;
+			int silentSamplesToAdd = std::min(maximumSilentSamples, (splitEnd - splitStart) / 2);
+			splitStart += silentSamplesToAdd;
+
 			result << splitAndTranscribeBefore_(splitStart, splitEnd) << "\n\n";
 			return true;
 		}
