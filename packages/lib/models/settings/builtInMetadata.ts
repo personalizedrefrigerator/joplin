b286bbd596938d2fcfc71105a315fcd2821791af
Henry Heino <personalizedrefrigerator@gmail.com>
2025-03-28 12:09:49 -0700

    Remove now-unused setting

--- packages/lib/models/settings/builtInMetadata.ts
+++ packages/lib/models/settings/builtInMetadata.ts
@@ -839,22 +839,6 @@ const builtInMetadata = (Setting: typeof SettingType) => {
 			isGlobal: true,
 		},
 
-		// Works around a bug in which additional space is visible beneath the toolbar on some devices.
-		// See https://github.com/laurent22/joplin/pull/6823
-		// No longer needed:
-		'editor.mobile.removeSpaceBelowToolbar': {
-			value: false,
-			type: SettingItemType.Bool,
-			section: 'note',
-			public: true,
-			appTypes: [AppType.Mobile],
-			show: (_settings: unknown) => false, // settings['editor.mobile.removeSpaceBelowToolbar'],
-			label: () => 'Remove extra space below the markdown toolbar',
-			description: () => 'Works around bug on some devices where the markdown toolbar does not touch the bottom of the screen.',
-			storage: SettingStorage.File,
-			isGlobal: true,
-		},
-
 		newTodoFocus: {
 			value: 'title',
 			type: SettingItemType.String,
