950a7cd57a6e740d34e052c838003b1ef2c5da59
Henry Heino <personalizedrefrigerator@gmail.com>
2025-04-01 12:33:10 -0700

    Desktop: Remove now-unnecessary store.dispatch

--- packages/app-desktop/gui/Root.tsx
+++ packages/app-desktop/gui/Root.tsx
@@ -89,11 +89,6 @@ async function initialize() {
 	});
 
 	store.dispatch({
-		type: 'EDITOR_PLUGIN_VIEW_IDS_CHANGED',
-		value: Setting.value('plugins.shownEditorViewIds'),
-	});
-
-	store.dispatch({
 		type: 'NOTE_VISIBLE_PANES_SET',
 		panes: Setting.value('noteVisiblePanes'),
 	});
