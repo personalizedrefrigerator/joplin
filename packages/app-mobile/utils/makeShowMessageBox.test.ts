807bcdcf9582805f5976184eed63d677060bc9a9
Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>
2025-04-24 00:50:35 -0700

    Web: Fix tapping outside alert/confirm dialogs doesn't dismiss them correctly (#12144)

--- packages/app-mobile/utils/makeShowMessageBox.test.ts
+++ packages/app-mobile/utils/makeShowMessageBox.test.ts
@@ -1,14 +1,15 @@
+import { MessageBoxType } from '@joplin/lib/shim';
 import { DialogControl } from '../components/DialogManager';
 import { PromptButtonSpec } from '../components/DialogManager/types';
 import makeShowMessageBox from './makeShowMessageBox';
 
-type OnPrompt = (buttons: PromptButtonSpec[])=> void;
+type OnPrompt = (buttons: PromptButtonSpec[], onDismiss: ()=> void)=> void;
 const makeMockDialogControl = (onPrompt: OnPrompt): DialogControl => {
 	return {
 		info: jest.fn(),
 		error: jest.fn(),
-		prompt: jest.fn((_title, _message, buttons) => {
-			onPrompt(buttons);
+		prompt: jest.fn((_title, _message, buttons, options) => {
+			onPrompt(buttons, options.onDismiss);
 		}),
 		showMenu: jest.fn(),
 	};
@@ -23,5 +24,17 @@ describe('makeShowMessageBox', () => {
 
 		const okButtonIndex = 0;
 		expect(await showMessageBox('test')).toBe(okButtonIndex);
+	});
+
+	test('should resolve to the index of the cancel button when cancelled', async () => {
+		const dialogControl = makeMockDialogControl((_buttons, onDismiss) => {
+			// Cancel
+			onDismiss();
+		});
+
+		const showMessageBox = makeShowMessageBox({ current: dialogControl });
+		expect(await showMessageBox('test')).toBe(1); // Cancel button index
+		// Should resolve to -1 when there is no cancel button
+		expect(await showMessageBox('test', { type: MessageBoxType.Error })).toBe(-1);
 	});
 });
