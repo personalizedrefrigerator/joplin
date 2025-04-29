0c26145bebc2eecf89959a550d199f5816d706ae
Henry Heino <personalizedrefrigerator@gmail.com>
2025-04-29 14:59:20 -0700

    WIP: Remove canvas dependency

--- packages/lib/jest.setup.js
+++ packages/lib/jest.setup.js
@@ -2,7 +2,6 @@ const { afterEachCleanUp } = require('./testing/test-utils.js');
 const { shimInit } = require('./shim-init-node.js');
 const sharp = require('sharp');
 const nodeSqlite = require('sqlite3');
-const pdfJs = require('pdfjs-dist');
 const packageInfo = require('./package.json');
 
 // Used for testing some shared components
@@ -10,7 +9,7 @@ const React = require('react');
 
 require('../../jest.base-setup.js')();
 
-shimInit({ sharp, nodeSqlite, pdfJs, React, appVersion: () => packageInfo.version });
+shimInit({ sharp, nodeSqlite, React, appVersion: () => packageInfo.version });
 
 global.afterEach(async () => {
 	await afterEachCleanUp();
