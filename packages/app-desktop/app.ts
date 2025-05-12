cc74be0833bdfb072cdc2dff99467b0ded58217e
Henry Heino <personalizedrefrigerator@gmail.com>
2025-05-12 07:23:53 -0700

    Add test

--- packages/app-desktop/app.ts
+++ packages/app-desktop/app.ts
@@ -149,10 +149,6 @@ class Application extends BaseApplication {
 			await AlarmService.updateNoteNotification(action.id, action.type === 'NOTE_DELETE');
 		}
 
-		if (action.type === 'SETTING_UPDATE_ONE' && action.key === 'featureFlag.autoUpdaterServiceEnabled' || action.type === 'SETTING_UPDATE_ALL') {
-			if (Setting.value('featureFlag.autoUpdaterServiceEnabled')) this.setupAutoUpdaterService();
-		}
-
 		const result = await super.generalMiddleware(store, next, action);
 		const newState = store.getState();
 
@@ -390,8 +386,6 @@ class Application extends BaseApplication {
 	}
 
 	private setupAutoUpdaterService() {
-		this.logger().info('Setting up auto-updater service...');
-
 		if (Setting.value('featureFlag.autoUpdaterServiceEnabled')) {
 			bridge().electronApp().initializeAutoUpdaterService(
 				Logger.create('AutoUpdaterService'),
@@ -418,6 +412,14 @@ class Application extends BaseApplication {
 		});
 	}
 
+	private async setupIntegrationTestUtils() {
+		// Events used by Playwright tests to quickly change the value of a setting.
+		ipcRenderer.on('testing--setSetting', (_event, key, value) => {
+			this.logger().info('Updating setting using testing API: %s = %s', key, JSON.stringify(value));
+			Setting.setValue(key, value);
+		});
+	}
+
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any -- Old code before rule was applied
 	public async start(argv: string[], startOptions: StartOptions = null): Promise<any> {
 		// If running inside a package, the command line, instead of being "node.exe <path> <flags>" is "joplin.exe <flags>" so
@@ -425,6 +427,8 @@ class Application extends BaseApplication {
 		if (!bridge().electronIsDev()) argv.splice(1, 0, '.');
 
 		argv = await super.start(argv, startOptions);
+
+		await this.setupIntegrationTestUtils();
 
 		bridge().setLogFilePath(Logger.globalLogger.logFilePath());
 
