a62fddcece06499343a20742aef9b5384e59c46a
Henry Heino <personalizedrefrigerator@gmail.com>
2025-05-23 15:35:34 -0700

    git rm .js files that should not be tracked by git

--- packages/lib/components/shared/ShareNoteDialog/onUnshareNoteClick.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ShareService_1 = require("../../../services/share/ShareService");
-const onUnshareNoteClick = async (event) => {
-    await ShareService_1.default.instance().unshareNote(event.noteId);
-    await ShareService_1.default.instance().refreshShares();
-};
-exports.default = onUnshareNoteClick;
-//# sourceMappingURL=onUnshareNoteClick.js.map
