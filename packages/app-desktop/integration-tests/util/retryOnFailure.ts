bebdfec3739af2a9cd227605e6a6b7d5183c94bb
Henry Heino <personalizedrefrigerator@gmail.com>
2025-06-11 12:52:37 -0700

    Chore: CI: Re-try opening the go-to-anything dialog on failure

---
+++ packages/app-desktop/integration-tests/util/retryOnFailure.ts
@@ -0,0 +1,19 @@
+
+interface Options {
+	maxRetries: number;
+}
+
+const retryOnFailure = async <T> (callback: ()=> Promise<T>, { maxRetries }: Options): Promise<T> => {
+	let lastError: Error|null = null;
+	for (let i = 0; i < maxRetries; i++) {
+		try {
+			return await callback();
+		} catch (error) {
+			lastError = error;
+		}
+	}
+
+	throw lastError;
+};
+
+export default retryOnFailure;
