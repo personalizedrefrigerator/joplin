a2b56802f087284bfd5d4b6ae5d6fe865fd6a338
Henry Heino <personalizedrefrigerator@gmail.com>
2025-06-30 08:35:53 -0700

    Fix combobox not hidden in compact mode after clicking on a result

--- packages/app-mobile/components/ComboBox.tsx
+++ packages/app-mobile/components/ComboBox.tsx
@@ -326,7 +326,11 @@ const ComboBox: React.FC<Props> = ({
 			propsOnItemSelectedRef.current(item);
 			setSearch('');
 		}
-	}, []);
+
+		if (!alwaysExpand) {
+			setShowSearchResults(false);
+		}
+	}, [setShowSearchResults, alwaysExpand]);
 
 	const baseId = useId();
 	const SearchResultWrapper = useSearchResultContainerComponent(
@@ -348,11 +352,7 @@ const ComboBox: React.FC<Props> = ({
 		if (item) {
 			onItemSelected(item);
 		}
-
-		if (!alwaysExpand) {
-			setShowSearchResults(false);
-		}
-	}, [onItemSelected, setShowSearchResults, alwaysExpand, results, selectedIndex]);
+	}, [onItemSelected, results, selectedIndex]);
 
 	// For now, onKeyPress only works on web.
 	// See https://github.com/react-native-community/discussions-and-proposals/issues/249
