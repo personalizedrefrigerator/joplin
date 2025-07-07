f5c86ee19df5622d06dfe14a7485c5558db00f9f
Henry Heino <personalizedrefrigerator@gmail.com>
2025-07-07 06:49:32 -0700

    Revert comment

--- packages/app-desktop/utils/sourceMapSetup.ts
+++ packages/app-desktop/utils/sourceMapSetup.ts
@@ -1,6 +1,6 @@
 
-// source-map-support can add some time to application startup.
-// Disable it unless explicitly requested:
+// source-map-support can add 1-3 seconds to the application startup
+// time -- disable it unless requested:
 if (process.env.JOPLIN_SOURCE_MAP_ENABLED) {
 	require('source-map-support').install();
 }
