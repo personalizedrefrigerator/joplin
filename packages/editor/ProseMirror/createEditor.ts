7ddb1b7d61e31018f802ba2f6f677cf6b3468f0b
Henry Heino <personalizedrefrigerator@gmail.com>
2025-07-24 11:33:41 -0700

    Revert "Trying to improve checklist support"
    This reverts commit 16f906398e8bf6070dd282576ef30c34d453d19a.

--- packages/editor/ProseMirror/createEditor.ts
+++ packages/editor/ProseMirror/createEditor.ts
@@ -23,7 +23,6 @@ import joplinEditorApiPlugin, { setEditorApi } from './plugins/joplinEditorApiPl
 import linkTooltipPlugin from './plugins/linkTooltipPlugin';
 import { RendererControl } from './types';
 import resourcePlaceholderPlugin, { onResourceDownloaded } from './plugins/resourcePlaceholderPlugin';
-import postprocessEditorOutput from './utils/postprocessEditorOutput';
 
 const createEditor = async (
 	parentElement: HTMLElement,
@@ -33,7 +32,6 @@ const createEditor = async (
 	const renderNodeToMarkup = (node: Node|DocumentFragment) => {
 		const element = document.createElement('div');
 		element.appendChild(node);
-		postprocessEditorOutput(element);
 		return renderer.renderHtmlToMarkup(element);
 	};
 
