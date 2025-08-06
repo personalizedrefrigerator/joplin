f658cbd95c3c5f3a4356e367b718fbfd10ce8a49
Henry Heino <personalizedrefrigerator@gmail.com>
2025-08-06 13:27:30 -0700

    Debugging: Add command to dump the content of a specific item

--- packages/app-cli/app/command-dump.ts
+++ packages/app-cli/app/command-dump.ts
@@ -3,21 +3,27 @@ import Folder from '@joplin/lib/models/Folder';
 import Note from '@joplin/lib/models/Note';
 import Tag from '@joplin/lib/models/Tag';
 import { FolderEntity, NoteEntity } from '@joplin/lib/services/database/types';
+import app from './app';
+import { ModelType } from '@joplin/lib/BaseModel';
+
+interface Options {
+	item?: string;
+}
 
 class Command extends BaseCommand {
 	public override usage() {
-		return 'dump';
+		return 'dump [item]';
 	}
 
 	public override description() {
-		return 'Dumps the complete database as JSON.';
+		return 'Dumps the complete database as JSON, or, if specified, just an item.';
 	}
 
 	public override hidden() {
 		return true;
 	}
 
-	public override async action() {
+	private async dumpDatabase_() {
 		let items: (NoteEntity | FolderEntity)[] = [];
 		const folders = await Folder.all();
 		for (let i = 0; i < folders.length; i++) {
@@ -35,6 +41,41 @@ class Command extends BaseCommand {
 		items = items.concat(tags);
 
 		this.stdout(JSON.stringify(items));
+	}
+
+	public override async action(options: Options) {
+		if (options.item) {
+			const displayItemIfType = async (type: ModelType) => {
+				const item = await app().loadItem(type, options.item);
+
+				if (item) {
+					this.stdout(JSON.stringify(item));
+					return true;
+				}
+				return false;
+			};
+
+			const possibleItemTypes = [
+				ModelType.Folder,
+				ModelType.Note,
+				ModelType.Resource,
+				ModelType.Tag,
+			];
+
+			let displayed = false;
+			for (const type of possibleItemTypes) {
+				displayed = await displayItemIfType(type);
+				if (displayed) {
+					break;
+				}
+			}
+
+			if (!displayed) {
+				throw new Error(`No such item found: ${options.item}`);
+			}
+		} else {
+			await this.dumpDatabase_();
+		}
 	}
 }
 
