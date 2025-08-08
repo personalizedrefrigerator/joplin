50ffe96b2c2da6cd7cdaffb040ce1209cc2e941f
Henry Heino <personalizedrefrigerator@gmail.com>
2025-08-08 14:35:25 -0700

    Revert refactoring change
    This refactoring change should be moved to a separate PR

--- packages/app-mobile/contentScripts/richTextEditorBundle/contentScript/convertHtmlToMarkdown.ts
+++ packages/app-mobile/contentScripts/richTextEditorBundle/contentScript/convertHtmlToMarkdown.ts
@@ -1,14 +1,27 @@
 const TurndownService = require('@joplin/turndown');
 const turndownPluginGfm = require('@joplin/turndown-plugin-gfm').gfm;
-const joplinTurndownOptions = require('@joplin/turndown/src/joplin-config.js');
 
 // Avoid using @joplin/lib/HtmlToMd here. HtmlToMd may cause several megabytes
 // of additional JavaScript and supporting data to be included.
 
 const convertHtmlToMarkdown = (html: string|HTMLElement) => {
-	const turndown = new TurndownService({
-		...joplinTurndownOptions,
-	});
+	const turndownOpts = {
+		headingStyle: 'atx',
+		codeBlockStyle: 'fenced',
+		preserveImageTagsWithSize: true,
+		preserveNestedTables: true,
+		preserveColorStyles: true,
+		bulletListMarker: '-',
+		emDelimiter: '*',
+		strongDelimiter: '**',
+		allowResourcePlaceholders: true,
+
+		// If soft-breaks are enabled, lines need to end with two or more spaces for
+		// trailing <br/>s to render. See
+		// https://github.com/laurent22/joplin/issues/8430
+		br: '  ',
+	};
+	const turndown = new TurndownService(turndownOpts);
 	turndown.use(turndownPluginGfm);
 	turndown.remove('script');
 	turndown.remove('style');
--- packages/lib/HtmlToMd.ts
+++ packages/lib/HtmlToMd.ts
@@ -1,7 +1,5 @@
 const TurndownService = require('@joplin/turndown');
 const turndownPluginGfm = require('@joplin/turndown-plugin-gfm').gfm;
-const baseOptions = require('@joplin/turndown/src/joplin-config.js');
-
 import markdownUtils from './markdownUtils';
 
 const pdfUrlRegex = /[\s\S]*?\.pdf$/i;
@@ -21,11 +19,21 @@ export default class HtmlToMd {
 	public parse(html: string|HTMLElement, options: ParseOptions = {}) {
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any -- Old code before rule was applied
 		const turndownOpts: any = {
-			...baseOptions,
+			headingStyle: 'atx',
 			anchorNames: options.anchorNames ? options.anchorNames.map(n => n.trim().toLowerCase()) : [],
+			codeBlockStyle: 'fenced',
 			preserveImageTagsWithSize: !!options.preserveImageTagsWithSize,
 			preserveNestedTables: !!options.preserveNestedTables,
 			preserveColorStyles: !!options.preserveColorStyles,
+			bulletListMarker: '-',
+			emDelimiter: '*',
+			strongDelimiter: '**',
+			allowResourcePlaceholders: true,
+
+			// If soft-breaks are enabled, lines need to end with two or more spaces for
+			// trailing <br/>s to render. See
+			// https://github.com/laurent22/joplin/issues/8430
+			br: '  ',
 
 			disableEscapeContent: 'disableEscapeContent' in options ? options.disableEscapeContent : false,
 		};
