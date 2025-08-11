e1dfba031ad399d14350d4c0e2edad6354f1a72f
Henry Heino <personalizedrefrigerator@gmail.com>
2025-08-11 10:18:52 -0700

    Revert changes to "dump" command
    While these changes were useful for debugging a fuzzer failure, they
    aren't directly related to adding/removing accounts in the sync fuzzer.

--- packages/app-cli/app/command-dump.ts
+++ packages/app-cli/app/command-dump.ts
@@ -3,27 +3,21 @@ import Folder from '@joplin/lib/models/Folder';
 import Note from '@joplin/lib/models/Note';
 import Tag from '@joplin/lib/models/Tag';
 import { FolderEntity, NoteEntity } from '@joplin/lib/services/database/types';
-import app from './app';
-import { ModelType } from '@joplin/lib/BaseModel';
-
-interface Options {
-	item?: string;
-}
 
 class Command extends BaseCommand {
 	public override usage() {
-		return 'dump [item]';
+		return 'dump';
 	}
 
 	public override description() {
-		return 'Dumps the complete database as JSON, or, if specified, just an item.';
+		return 'Dumps the complete database as JSON.';
 	}
 
 	public override hidden() {
 		return true;
 	}
 
-	private async dumpDatabase_() {
+	public override async action() {
 		let items: (NoteEntity | FolderEntity)[] = [];
 		const folders = await Folder.all();
 		for (let i = 0; i < folders.length; i++) {
@@ -41,41 +35,6 @@ class Command extends BaseCommand {
 		items = items.concat(tags);
 
 		this.stdout(JSON.stringify(items));
-	}
-
-	public override async action(options: Options) {
-		if (options.item) {
-			const displayItemIfType = async (type: ModelType) => {
-				const item = await app().loadItem(type, options.item);
-
-				if (item) {
-					this.stdout(JSON.stringify(item));
-					return true;
-				}
-				return false;
-			};
-
-			const possibleItemTypes = [
-				ModelType.Folder,
-				ModelType.Note,
-				ModelType.Resource,
-				ModelType.Tag,
-			];
-
-			let displayed = false;
-			for (const type of possibleItemTypes) {
-				displayed = await displayItemIfType(type);
-				if (displayed) {
-					break;
-				}
-			}
-
-			if (!displayed) {
-				throw new Error(`No such item found: ${options.item}`);
-			}
-		} else {
-			await this.dumpDatabase_();
-		}
 	}
 }
 
