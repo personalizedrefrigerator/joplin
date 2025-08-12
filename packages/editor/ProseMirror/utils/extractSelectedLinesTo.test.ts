ae81824ad4b624edbed30e9d180f14a93e4eec1e
Henry Heino <personalizedrefrigerator@gmail.com>
2025-08-12 14:51:17 -0700

    Refactoring: Move the new test.each block to the start of the describe
    Goal: Make the pull request diff easier to review

--- packages/editor/ProseMirror/utils/extractSelectedLinesTo.test.ts
+++ packages/editor/ProseMirror/utils/extractSelectedLinesTo.test.ts
@@ -4,51 +4,6 @@ import extractSelectedLinesTo from './extractSelectedLinesTo';
 import schema from '../schema';
 
 describe('extractSelectedLinesTo', () => {
-	test('should extract multiple lines in the same paragraph to a new paragraph', () => {
-		const view = createTestEditor({
-			html: '<p>Line 1<br>Line 2<br>Line 3<br>Line 4</p>',
-		});
-		view.dispatch(view.state.tr.setSelection(
-			TextSelection.create(
-				view.state.doc,
-				// ...from the middle of the second line...
-				'<Line 1|Line'.length,
-				// ...to the end of the third line
-				'<Line 1|Line 2|Line 3'.length,
-			),
-		));
-
-		const { transaction } = extractSelectedLinesTo(
-			{ type: schema.nodes.paragraph, attrs: { } },
-			view.state.tr,
-			view.state.selection,
-		);
-
-		view.dispatch(transaction);
-
-		// The section of the document containing the cursor should now be a new line
-		expect(view.state.doc.toJSON()).toMatchObject({
-			content: [
-				{
-					type: 'paragraph',
-					content: [{ type: 'text', text: 'Line 1' }],
-				},
-				{
-					type: 'paragraph',
-					content: [
-						{ type: 'text', text: 'Line 2' },
-						{ type: 'hard_break' },
-						{ type: 'text', text: 'Line 3' },
-					],
-				},
-				{
-					type: 'paragraph',
-					content: [{ type: 'text', text: 'Line 4' }],
-				},
-			],
-		});
-	});
-
 	test.each([
 		{
 			label: 'should extract a single line containing the cursor to a heading',
@@ -176,5 +131,50 @@ describe('extractSelectedLinesTo', () => {
 		// All of the tests in this group expect a single cursor
 		expect(view.state.selection.empty).toBe(true);
 		expect(view.state.selection.from).toBe(expected.cursorPosition);
+	});
+
+	test('should extract multiple lines in the same paragraph to a new paragraph', () => {
+		const view = createTestEditor({
+			html: '<p>Line 1<br>Line 2<br>Line 3<br>Line 4</p>',
+		});
+		view.dispatch(view.state.tr.setSelection(
+			TextSelection.create(
+				view.state.doc,
+				// ...from the middle of the second line...
+				'<Line 1|Line'.length,
+				// ...to the end of the third line
+				'<Line 1|Line 2|Line 3'.length,
+			),
+		));
+
+		const { transaction } = extractSelectedLinesTo(
+			{ type: schema.nodes.paragraph, attrs: { } },
+			view.state.tr,
+			view.state.selection,
+		);
+
+		view.dispatch(transaction);
+
+		// The section of the document containing the cursor should now be a new line
+		expect(view.state.doc.toJSON()).toMatchObject({
+			content: [
+				{
+					type: 'paragraph',
+					content: [{ type: 'text', text: 'Line 1' }],
+				},
+				{
+					type: 'paragraph',
+					content: [
+						{ type: 'text', text: 'Line 2' },
+						{ type: 'hard_break' },
+						{ type: 'text', text: 'Line 3' },
+					],
+				},
+				{
+					type: 'paragraph',
+					content: [{ type: 'text', text: 'Line 4' }],
+				},
+			],
+		});
 	});
 });
