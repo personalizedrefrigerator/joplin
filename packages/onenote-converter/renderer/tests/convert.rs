2aa73a03c23d8881a7a0337611d0db432491fc4f
Henry Heino <personalizedrefrigerator@gmail.com>
2025-10-07 10:52:39 -0700

    Add renderer test using sample data

--- packages/onenote-converter/renderer/tests/convert.rs
+++ packages/onenote-converter/renderer/tests/convert.rs
@@ -8,19 +8,20 @@ struct TestResources {
 }
 
 fn setup(test_id: &str) -> TestResources {
-    let output_dir = fs::canonicalize(
-        PathBuf::from("./test-output").join(test_id)
-    ).unwrap();
+    let output_dir = PathBuf::from("./test-output").join(test_id);
 
     if output_dir.exists() {
         fs::remove_dir_all(&output_dir).unwrap();
     }
     fs::create_dir_all(&output_dir).unwrap();
 
-    let test_data_dir = fs::canonicalize(PathBuf::from("../test-data/")).unwrap();
+    let test_data_dir = PathBuf::from("../test-data/");
     assert!(test_data_dir.exists());
 
-    TestResources { output_dir, test_data_dir }
+    TestResources {
+        output_dir: fs::canonicalize(output_dir).unwrap(),
+        test_data_dir: fs::canonicalize(test_data_dir).unwrap(),
+    }
 }
 
 #[test]
@@ -45,3 +46,27 @@ fn convert_web_export() {
             .exists()
     );
 }
+
+#[test]
+fn convert_desktop_export() {
+    let TestResources { output_dir, test_data_dir } = setup("desktop_export");
+    let test_data_dir = test_data_dir.join("onenote-2016");
+
+    convert(
+        &test_data_dir.join("OneWithFileData.one").to_string_lossy(),
+        &output_dir.to_string_lossy(),
+        &test_data_dir.to_string_lossy(),
+    )
+    .unwrap();
+
+    // Should create a table of contents file
+    assert!(output_dir.join("OneWithFileData.html").exists());
+    // Should convert the input page to an HTML file
+    assert!(
+        output_dir
+            .join("OneWithFileData")
+            .join("Untitled Page 1.html")
+            .exists()
+    );
+}
+
