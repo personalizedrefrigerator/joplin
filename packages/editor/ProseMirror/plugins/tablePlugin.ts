cdfd466d94cd8c5182fb1c97467fef1acc66d409
Henry Heino <personalizedrefrigerator@gmail.com>
2025-10-08 14:34:16 -0700

    Revert "Remove changes specific to the table editor"
    This reverts commit fd02dc8d0b046f0a4818982968091a1e8cc1535d.

---
+++ packages/editor/ProseMirror/plugins/tablePlugin.ts
@@ -0,0 +1,15 @@
+import { addColumnAfter, addRowAfter, deleteColumn, deleteRow, deleteTable, tableEditing } from 'prosemirror-tables';
+import createFloatingButtonPlugin, { ToolbarPosition } from './utils/createFloatingButtonPlugin';
+
+const tablePlugin = [
+	tableEditing({ allowTableNodeSelection: true }),
+	createFloatingButtonPlugin('table', [
+		{ label: (_) => _('Add row'), command: () => addRowAfter },
+		{ label: (_) => _('Add column'), command: () => addColumnAfter },
+		{ label: (_) => _('Delete row'), command: () => deleteRow },
+		{ label: (_) => _('Delete column'), command: () => deleteColumn },
+		{ label: (_) => _('Delete table'), command: () => deleteTable },
+	], ToolbarPosition.TopLeftOutside),
+];
+
+export default tablePlugin;
