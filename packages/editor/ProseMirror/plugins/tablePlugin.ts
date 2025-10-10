ff8f4ad69b6ebcab145906b494c4591db71cb4b2
Henry Heino <personalizedrefrigerator@gmail.com>
2025-10-10 16:54:32 -0700

    Refactoring: Change import names to avoid ambiguity

--- packages/editor/ProseMirror/plugins/tablePlugin.ts
+++ packages/editor/ProseMirror/plugins/tablePlugin.ts
@@ -1,17 +1,17 @@
 import { addColumnAfter, addRowAfter, deleteColumn, deleteRow, tableEditing } from 'prosemirror-tables';
 import createFloatingButtonPlugin, { ToolbarPosition } from './utils/createFloatingButtonPlugin';
-import addColumnRight from '../vendor/icons/addColumnRight';
-import addRowBelow from '../vendor/icons/addRowBelow';
-import removeRow from '../vendor/icons/removeRow';
-import removeColumn from '../vendor/icons/removeColumn';
+import addColumnRightIcon from '../vendor/icons/addColumnRight';
+import addRowBelowIcon from '../vendor/icons/addRowBelow';
+import removeRowIcon from '../vendor/icons/removeRow';
+import removeColumnIcon from '../vendor/icons/removeColumn';
 
 const tablePlugin = [
 	tableEditing({ allowTableNodeSelection: true }),
 	createFloatingButtonPlugin('table', [
-		{ icon: addRowBelow, label: (_) => _('Add row'), command: () => addRowAfter },
-		{ icon: addColumnRight, label: (_) => _('Add column'), command: () => addColumnAfter },
-		{ icon: removeRow, label: (_) => _('Delete row'), command: () => deleteRow },
-		{ icon: removeColumn, label: (_) => _('Delete column'), command: () => deleteColumn },
+		{ icon: addRowBelowIcon, label: (_) => _('Add row'), command: () => addRowAfter },
+		{ icon: addColumnRightIcon, label: (_) => _('Add column'), command: () => addColumnAfter },
+		{ icon: removeRowIcon, label: (_) => _('Delete row'), command: () => deleteRow },
+		{ icon: removeColumnIcon, label: (_) => _('Delete column'), command: () => deleteColumn },
 	], ToolbarPosition.TopLeftOutside),
 ];
 
