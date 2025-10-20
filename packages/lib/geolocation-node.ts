21adbef0e7e3c577e0bc1d0bbfa067a9a8a4222f
Henry Heino <personalizedrefrigerator@gmail.com>
2025-10-19 19:10:48 -0700

    Windows: Add system geolocation provider

--- packages/lib/geolocation-node.ts
+++ packages/lib/geolocation-node.ts
@@ -1,5 +1,6 @@
 import Logger from '@joplin/utils/Logger';
 import shim from './shim';
+import { execCommand } from '@joplin/utils';
 
 const logger = Logger.create('geolocation-node');
 
@@ -27,6 +28,71 @@ const fetchJson = async (url: string): Promise<any> => {
 };
 
 const geoipServices: Record<string, GeoipService> = {
+	os: async (): Promise<CurrentPositionResponse> => {
+		if (process.platform === 'win32') {
+			const response = await execCommand([
+				'PowerShell.exe',
+				'-Command',
+				// Referenced:
+				// - https://stackoverflow.com/a/46287884
+				// - https://learn.microsoft.com/en-us/dotnet/api/system.device.location.geocoordinatewatcher?view=netframework-4.8.1
+				`
+					# Required for access to System.
+					Add-Type -AssemblyName System.Device
+
+					$geo = [System.Device.Location.GeoCoordinateWatcher]::new()
+					$suppressPermissionPrompt = $false
+					$geo.TryStart(
+						$suppressPermissionPrompt,
+						[System.TimeSpan]::FromMilliseconds(1000)
+					)
+
+					while ($geo.Permission -ne "Denied" -and $geo.Position.Location.IsUnknown) {
+						Start-Sleep -Milliseconds 50
+					}
+
+					if ($geo.Permission -eq "Denied") {
+						Write-Error "Not permitted to access location."
+					} else {
+						$location = $geo.Position.Location
+						$latitude = $location.Latitude
+						$longitude = $location.Longitude
+						$altitude = $location.Altitude
+
+						# NaN can't be parsed by JS
+						if ($altitude -eq [Double].NaN) {
+							$altitude = $false
+						}
+
+						@{
+							Latitude = $latitude
+							Longitude = $longitude
+							Altitude = $altitude
+						} | ConvertTo-Json | Write-Output
+					}
+				`,
+			], { quiet: true });
+			const responseData = JSON.parse(response.replace(/^True/, ''));
+
+			const getResponseProperty = (propertyName: string) => {
+				if (!(propertyName in responseData) || typeof responseData[propertyName] !== 'number') {
+					throw new Error(`Missing or invalid property: ${propertyName} in response (response keys: ${JSON.stringify(Object.keys(responseData))})`);
+				}
+				return responseData[propertyName];
+			};
+
+			return {
+				timestamp: Date.now(),
+				coords: {
+					latitude: getResponseProperty('Latitude'),
+					longitude: getResponseProperty('Longitude'),
+					altitude: getResponseProperty('Altitude'),
+				},
+			};
+		} else {
+			throw new Error('OS geolocation is not supported on this platform');
+		}
+	},
 
 	ipwhois: async (): Promise<CurrentPositionResponse> => {
 		const r = await fetchJson('https://ipwho.is/');
@@ -42,19 +108,8 @@ const geoipServices: Record<string, GeoipService> = {
 		};
 	},
 
-	geoplugin: async (): Promise<CurrentPositionResponse> => {
-		const r = await fetchJson('http://www.geoplugin.net/json.gp');
-		if (!('geoplugin_latitude' in r) || !('geoplugin_longitude' in r)) throw new Error(`Invalid geolocation response: ${r ? JSON.stringify(r) : '<null>'}`);
-
-		return {
-			timestamp: Date.now(),
-			coords: {
-				longitude: Number(r.geoplugin_longitude),
-				altitude: 0,
-				latitude: Number(r.geoplugin_latitude),
-			},
-		};
-	},
+	// October 2025: geoplugin.net no longer offers a free IP resolution service and has been
+	// removed.
 
 };
 
