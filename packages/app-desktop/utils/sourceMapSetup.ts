ca14c265aaff96a97c823d56725b634d3b5aae85
Henry Heino <personalizedrefrigerator@gmail.com>
2025-10-30 09:11:52 -0700

    Chore: Desktop: Enable source maps for error reporting by default

--- packages/app-desktop/utils/sourceMapSetup.ts
+++ packages/app-desktop/utils/sourceMapSetup.ts
@@ -1,6 +1,6 @@
 
 // source-map-support can add 1-3 seconds to the application startup
 // time -- disable it unless requested:
-if (process.env.JOPLIN_SOURCE_MAP_ENABLED) {
+if (!process.env.JOPLIN_SOURCE_MAP_DISABLED) {
 	require('source-map-support').install();
 }
