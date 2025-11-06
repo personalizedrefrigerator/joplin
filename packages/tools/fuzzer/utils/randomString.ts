07a6d644c6a403df4eb4a4b231ffcd500ef24821
Henry Heino <personalizedrefrigerator@gmail.com>
2025-11-06 14:54:59 -0800

    Add back additional string postprocessing

--- packages/tools/fuzzer/utils/randomString.ts
+++ packages/tools/fuzzer/utils/randomString.ts
@@ -9,7 +9,9 @@ const randomString = (nextRandomInteger: OnRandomInt) => (length: number) => {
 	let text = String.fromCharCode(...charCodes);
 	// Normalize to avoid differences when reading/writing content from Joplin clients.
 	// TODO: Can the note comparison logic be adjusted to remove some of this?
-	text = text.replace(/\p{C}/ug, '-'); // Control characters
+	text = text.normalize();
+	text = text.replace(/[\r\b\f\v\0\u007F]/g, '!');
+	text = text.replace(/\p{C}/ug, '-'); // Other control characters
 
 	// Remove invalid UTF-8
 	text = new TextDecoder().decode(new TextEncoder().encode(text));
