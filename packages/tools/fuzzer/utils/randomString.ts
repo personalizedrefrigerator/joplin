10a30b75e34ee8a7a8bbc55b66741ae96e5381fe
Henry Heino <personalizedrefrigerator@gmail.com>
2025-11-06 14:39:49 -0800

    Refactor randomString.ts, remove unnecessary text normalization

--- packages/tools/fuzzer/utils/randomString.ts
+++ packages/tools/fuzzer/utils/randomString.ts
@@ -1,17 +1,15 @@
 type OnRandomInt = (low: number, high: number)=> number;
 
-const randomString = (generator: OnRandomInt) => (length: number) => {
+const randomString = (nextRandomInteger: OnRandomInt) => (length: number) => {
 	const charCodes = [];
 	for (let i = 0; i < length; i++) {
-		charCodes.push(generator(0, 0xFFFF));
+		charCodes.push(nextRandomInteger(0, 0xFFFF));
 	}
 
 	let text = String.fromCharCode(...charCodes);
 	// Normalize to avoid differences when reading/writing content from Joplin clients.
 	// TODO: Can the note comparison logic be adjusted to remove some of this?
-	text = text.normalize();
-	text = text.replace(/[\r\b\f\v\0\u007F]/g, '!');
-	text = text.replace(/\p{C}/ug, '-'); // Other control characters
+	text = text.replace(/\p{C}/ug, '-'); // Control characters
 
 	// Remove invalid UTF-8
 	text = new TextDecoder().decode(new TextEncoder().encode(text));
