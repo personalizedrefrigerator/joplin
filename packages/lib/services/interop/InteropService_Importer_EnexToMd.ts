733c07a8c8e80b18a3348e06e01df5aba187a5b9
Henry Heino <personalizedrefrigerator@gmail.com>
2025-11-21 10:07:47 -0800

    Revert changes to InteropService_Importer_Enex*

--- packages/lib/services/interop/InteropService_Importer_EnexToMd.ts
+++ packages/lib/services/interop/InteropService_Importer_EnexToMd.ts
@@ -7,35 +7,27 @@ import { fileExtension, rtrimSlashes } from '../../path-utils';
 import shim from '../../shim';
 const { filename } = require('../../path-utils');
 
-const createDestinationFolder = async (importFilepath: string, parentId: string|undefined) => {
-	const title = await Folder.findUniqueItemTitle(filename(importFilepath));
-	return await Folder.save({ title, parent_id: parentId ?? '' });
-};
-
 const doImportEnex = async (destFolder: FolderEntity, sourcePath: string, options: ImportOptions) => {
 	if (!destFolder) {
-		destFolder = await createDestinationFolder(sourcePath, undefined);
+		const folderTitle = await Folder.findUniqueItemTitle(filename(sourcePath));
+		destFolder = await Folder.save({ title: folderTitle });
 	}
 
 	await importEnex(destFolder.id, sourcePath, options);
 };
 
-export const enexImporterExec = async (result: ImportExportResult, destinationFolder: FolderEntity, sourcePath: string, fileExtensions: string[], options: ImportOptions) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any -- Old code before rule was applied
+export const enexImporterExec = async (result: ImportExportResult, destinationFolder: FolderEntity, sourcePath: string, fileExtensions: string[], options: any) => {
 	sourcePath = rtrimSlashes(sourcePath);
 
 	if (await shim.fsDriver().isDirectory(sourcePath)) {
 		const stats = await shim.fsDriver().readDirStats(sourcePath);
-
 		for (const stat of stats) {
 			const fullPath = `${sourcePath}/${stat.path}`;
 			if (!fileExtensions.includes(fileExtension(fullPath).toLowerCase())) continue;
 
-			// When importing a directory, avoid putting the imported notes directly in the parent
-			// folders (each entry in the directory to import should be given its own Joplin folder).
-			const destinationSubfolder = destinationFolder ? await createDestinationFolder(fullPath, destinationFolder.id) : null;
-
 			try {
-				await doImportEnex(destinationSubfolder, fullPath, options);
+				await doImportEnex(null, fullPath, options);
 			} catch (error) {
 				result.warnings.push(`When importing "${fullPath}": ${error.message}`);
 			}
@@ -49,11 +41,6 @@ export const enexImporterExec = async (result: ImportExportResult, destinationFo
 
 export default class InteropService_Importer_EnexToMd extends InteropService_Importer_Base {
 	public async exec(result: ImportExportResult) {
-		let destinationFolder = this.options_.destinationFolder;
-		if (!destinationFolder && this.options_.destinationFolderId) {
-			destinationFolder = await Folder.load(this.options_.destinationFolderId);
-		}
-
 		return enexImporterExec(
 			result,
 			this.options_.destinationFolder,
