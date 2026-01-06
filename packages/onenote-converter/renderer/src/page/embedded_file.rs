ca35ee33312775d2332fb23f0695af2852935105
Henry Heino <personalizedrefrigerator@gmail.com>
2026-01-06 12:48:50 -0800

    Desktop: OneNote importer: Work around video import issue

--- packages/onenote-converter/renderer/src/page/embedded_file.rs
+++ packages/onenote-converter/renderer/src/page/embedded_file.rs
@@ -25,10 +25,12 @@ impl<'a> Renderer<'a> {
 
         let file_type = Self::guess_type(file);
         match file_type {
-            // TODO: we still don't have support for the audio tag on html notes https://github.com/laurent22/joplin/issues/11939
+            // TODO: we still don't have support for the audio tag on html notes https://github.com/laurent22/joplin/issues/11939.
+            // TODO: Joplin currently can't import <video> resources in HTML notes, render as links
+            //       instead.
             // FileType::Audio => content = format!("<audio class=\"media-player media-audio\"controls><source src=\"{}\" type=\"audio/x-wav\"></source></audio>", filename),
-            FileType::Video => content = format!("<video controls src=\"{}\" {}></video>", filename, styles.to_html_attr()),
-            FileType::Unknown | FileType::Audio => {
+            // FileType::Video => content = format!("<video controls src=\"{}\" {}></video>", filename, styles.to_html_attr()),
+            FileType::Unknown | FileType::Audio | FileType::Video => {
                 styles.set("font-size", "11pt".into());
                 styles.set("line-height", "17px".into());
                 let style_attr = styles.to_html_attr();
