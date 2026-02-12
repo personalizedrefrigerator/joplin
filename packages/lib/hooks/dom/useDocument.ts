b98199d3cfde6a2b580d655428ebc7ebf681f752
Henry Heino <personalizedrefrigerator@gmail.com>
2026-02-11 17:11:29 -0800

    Lazily evaluate shim.react()

--- packages/lib/hooks/dom/useDocument.ts
+++ packages/lib/hooks/dom/useDocument.ts
@@ -1,11 +1,12 @@
 import shim from '../../shim';
 
-const { useMemo } = shim.react();
 
 // Returns the Document that contains [elementRef]. This is useful when a component
 // can be rendered in secondary windows and needs to access the component's container
 // document.
 const useDocument = (elementRef: Element|null) => {
+	const { useMemo } = shim.react();
+
 	return useMemo(() => {
 		return (elementRef ?? null)?.getRootNode() as Document|null;
 	}, [elementRef]);
