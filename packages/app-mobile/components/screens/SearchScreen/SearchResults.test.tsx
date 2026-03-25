4c321fb81245c76033d01a601c7d6c26c38980d6
Henry Heino <personalizedrefrigerator@gmail.com>
2026-03-25 10:55:47 -0700

    Chore: CI: Fix test warnings

--- packages/app-mobile/components/screens/SearchScreen/SearchResults.test.tsx
+++ packages/app-mobile/components/screens/SearchScreen/SearchResults.test.tsx
@@ -5,7 +5,7 @@ import { setupDatabaseAndSynchronizer, switchClient } from '@joplin/lib/testing/
 import createMockReduxStore from '../../../utils/testing/createMockReduxStore';
 import setupGlobalStore from '../../../utils/testing/setupGlobalStore';
 import Note from '@joplin/lib/models/Note';
-import { render, screen } from '../../../utils/testing/testingLibrary';
+import { act, render, screen } from '../../../utils/testing/testingLibrary';
 import SearchResults from './SearchResults';
 import SearchEngine from '@joplin/lib/services/search/SearchEngine';
 import Folder from '@joplin/lib/models/Folder';
@@ -44,7 +44,9 @@ describe('SearchResult', () => {
 		await createNotes(noteCount);
 
 		render(<WrappedSearchResults query='abcd' paused={false}/>);
-		const items = await screen.findAllByText(/abcd \d\d?\d?/);
-		expect(items.length).toBe(noteCount);
+		await act(async () => {
+			const items = await screen.findAllByText(/abcd \d\d?\d?/);
+			expect(items.length).toBe(noteCount);
+		});
 	});
 });
