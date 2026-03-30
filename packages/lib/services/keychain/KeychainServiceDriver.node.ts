3822965d10e0c17ebdfe117d20b3ad238f38d429
Henry Heino <personalizedrefrigerator@gmail.com>
2026-03-30 10:59:37 -0700

    Desktop: Disallow writing passwords to keytar

--- packages/lib/services/keychain/KeychainServiceDriver.node.ts
+++ packages/lib/services/keychain/KeychainServiceDriver.node.ts
@@ -3,12 +3,25 @@ import shim from '../../shim';
 
 export default class KeychainServiceDriver extends KeychainServiceDriverBase {
 	public override readonly driverId: string = 'node-keytar';
+	private disallowNewPasswords_ = false;
+
+	public constructor(appId: string, clientId: string) {
+		super(appId, clientId);
+
+		if (shim.isElectron()) {
+			this.disallowNewPasswords_ = true;
+		}
+	}
 
 	public async supported(): Promise<boolean> {
 		return !!shim.keytar?.();
 	}
 
 	public async setPassword(name: string, password: string): Promise<boolean> {
+		if (this.disallowNewPasswords_) {
+			return false;
+		}
+
 		await shim.keytar().setPassword(`${this.appId}.${name}`, `${this.clientId}@joplin`, password);
 		return true;
 	}
