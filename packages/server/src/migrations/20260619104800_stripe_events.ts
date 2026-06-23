a6231d1ea2823eb30a21210193c0fab9a72be48f
Henry Heino <personalizedrefrigerator@gmail.com>
2026-06-23 13:39:46 -0700

    Mark the id key with .primary

--- packages/server/src/migrations/20260619104800_stripe_events.ts
+++ packages/server/src/migrations/20260619104800_stripe_events.ts
@@ -2,7 +2,7 @@ import { DbConnection } from '../db';
 
 export const up = async (db: DbConnection) => {
 	await db.schema.createTable('stripe_events', (table) => {
-		table.string('id', 32).unique().notNullable();
+		table.string('id', 32).unique().primary().notNullable();
 		table.string('stripe_id', 64).unique().notNullable();
 		table.bigInteger('created_time').notNullable();
 		table.bigInteger('updated_time').notNullable();
