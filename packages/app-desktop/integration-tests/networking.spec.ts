99910958a9cca89b2aa6f2ce52bf946959273da4
Henry Heino <personalizedrefrigerator@gmail.com>
2026-09-10 17:02:18 -0700

    Add failing networking test for self-signed cert support

--- packages/app-desktop/integration-tests/networking.spec.ts
+++ packages/app-desktop/integration-tests/networking.spec.ts
@@ -2,7 +2,8 @@ import { test, expect } from './util/test';
 import MainScreen from './models/MainScreen';
 import type shim from '@joplin/lib/shim';
 import createLocalhostServer from '@joplin/lib/testing/createLocalhostServer';
-import { readFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
+import setSettingValue from './util/setSettingValue';
 import { join } from 'node:path';
 import { FetchOptions } from '@joplin/lib/shim';
 
@@ -18,45 +19,54 @@ declare const window: ExtendedWindow;
 // shim.fetch is based on Undici, which needs special bundling to run in Electron.
 test.describe('networking', () => {
 
-	test('shim.fetch and shim.fetchBlob should fetch an http URL', async ({ mainWindow, profileDirectory }) => {
-		const server = await createLocalhostServer((request, response) => {
-			response.writeHead(200, { 'content-type': 'application/json' });
-			response.end(JSON.stringify({ success: 1, method: request.method }));
-		}, { https: false });
-		const targetUrl = `${server.baseUrl}/ping`;
+	for (const protocol of ['http' as const, 'https' as const]) {
+		test(`shim.fetch and shim.fetchBlob should fetch an ${protocol} URL`, async ({ mainWindow, electronApp, profileDirectory }) => {
+			const server = await createLocalhostServer((request, response) => {
+				response.writeHead(200, { 'content-type': 'application/json' });
+				response.end(JSON.stringify({ success: 1, method: request.method }));
+			}, { https: protocol === 'https' });
+			const targetUrl = `${server.baseUrl}/ping`;
 
-		const mainScreen = await new MainScreen(mainWindow).setup();
-		await mainScreen.waitFor();
+			const tempDir = join(profileDirectory, 'tmp');
+			if (protocol === 'https') {
+				const certPath = join(profileDirectory, 'test__serverCert.pem');
+				await writeFile(certPath, server.cert, 'utf-8');
+				await setSettingValue(electronApp, mainWindow, 'net.customCertificates', certPath);
+			}
 
-		const sendFetchRequest = (url: string, options: FetchOptions) => (
-			mainWindow.evaluate(async ({ url, options }) => {
-				const response = await window.joplin.shim.fetch(url, options);
-				return {
-					ok: response.ok,
-					json: await response.json(),
-				};
-			}, { url, options })
-		);
-		expect(await sendFetchRequest(targetUrl, { method: 'GET' })).toMatchObject({
-			ok: true,
-			json: { success: 1, method: 'GET' },
-		});
-		expect(await sendFetchRequest(targetUrl, { method: 'POST' })).toMatchObject({
-			ok: true,
-			json: { success: 1, method: 'POST' },
-		});
+			const mainScreen = await new MainScreen(mainWindow).setup();
+			await mainScreen.waitFor();
 
-		const tempDir = join(profileDirectory, 'tmp');
-		const downloadedFile = await mainWindow.evaluate(async ([targetUrl, tempDir]) => {
-			const outputPath = `${tempDir}/test.txt`;
-			await window.joplin.shim.fetchBlob(targetUrl, { path: outputPath });
-			return outputPath;
-		}, [targetUrl, tempDir]);
-		expect(JSON.parse(await readFile(downloadedFile, 'utf-8'))).toMatchObject({
-			success: 1,
-			method: 'GET',
+			const sendFetchRequest = (url: string, options: FetchOptions) => (
+				mainWindow.evaluate(async ({ url, options }) => {
+					const response = await window.joplin.shim.fetch(url, options);
+					return {
+						ok: response.ok,
+						json: await response.json(),
+					};
+				}, { url, options })
+			);
+			expect(await sendFetchRequest(targetUrl, { method: 'GET' })).toMatchObject({
+				ok: true,
+				json: { success: 1, method: 'GET' },
+			});
+			expect(await sendFetchRequest(targetUrl, { method: 'POST' })).toMatchObject({
+				ok: true,
+				json: { success: 1, method: 'POST' },
+			});
+
+			const downloadedFile = await mainWindow.evaluate(async ([targetUrl, tempDir]) => {
+				const outputPath = `${tempDir}/test.txt`;
+				await window.joplin.shim.fetchBlob(targetUrl, { path: outputPath });
+				return outputPath;
+			}, [targetUrl, tempDir]);
+			expect(JSON.parse(await readFile(downloadedFile, 'utf-8'))).toMatchObject({
+				success: 1,
+				method: 'GET',
+			});
 		});
-	});
+	}
+
 
 });
 
